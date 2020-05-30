e5b51ae1968222ef7684cb481de4a4e71e9b0d26
jideabdqudus <jideabdqudus@gmail.com>
2020-05-30 21:25:56 +0100

    Connected MOngoDB

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 //Initialize Express
 const app = express();
+
+//Conncect DB
+connectDB();
 
 //Sending data to the PORT
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the Contact-Kepper API" })
 );
 
-
 //Define Routes
 
-app.use("/api/users", require("./routes/users"))
-app.use("/api/auth", require("./routes/auth"))
-app.use("/api/contacts", require("./routes/contacts"))
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 //Set Port
 const PORT = process.env.PORT || 5000;
