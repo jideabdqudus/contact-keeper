d95254d5ade18918a25b2e2301348b2d3a6cdcdb
jideabdqudus <jideabdqudus@gmail.com>
2020-05-30 20:08:24 +0100

    New file Structure & Build

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
 
+//Initialize Express
 const app = express();
 
-// Connect Database
-connectDB();
+//Sending data to the PORT
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the Contact-Kepper API" })
+);
 
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-// Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
-
+//Set Port
 const PORT = process.env.PORT || 5000;
 
+//Console run
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
