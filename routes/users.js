8aca3d81b12373a31d483e16562c12fd30b73ca5
jideabdqudus <jideabdqudus@gmail.com>
2020-05-31 09:32:50 +0100

    Validation for Models

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs")
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User.js");
@@ -19,12 +20,36 @@ router.post(
       "Please enteer a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errrors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("Passed");
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt)
+
+      await user.save()
+
+      res.send("User Saved")
+    } catch (error) {
+      console.error(err.message)
+      res.status(500).send("Server Error")
+    }
   }
 );
 
