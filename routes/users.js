154c3f43d7bfe4b11808bb4c6d99e3ed1eb0f215
jideabdqudus <jideabdqudus@gmail.com>
2020-06-03 18:51:54 +0100

    JWT Token

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator/check");
-
+const config = require("config");
 const User = require("../models/User");
 
 // @ route  POST api/users
@@ -45,7 +46,23 @@ router.post(
 
       await user.save();
 
-      res.send("User Saved");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
