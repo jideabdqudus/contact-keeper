dd62895e953b1aeb31ac3da8215e7729f9a81ddb
jideabdqudus <jideabdqudus@gmail.com>
2020-06-03 20:00:17 +0100

    Get Logged in Route

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator/check");
 const config = require("config");
+const auth = require("../middleware/auth")
 const User = require("../models/User");
 
 // @ route  GET api/auth
@@ -11,8 +12,14 @@ const User = require("../models/User");
 //@access   Private
 
 //Registerin a user, hence a post request.
-router.get("/", (req, res) => {
-  res.send("Get logged in User");
+router.get("/", auth, async(req, res) => {
+  try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 //@ route   POST api/auth
