9e919e665aa10494a72e303b76c60e4034a86196
jideabdqudus <jideabdqudus@gmail.com>
2020-06-04 13:20:27 +0100

    App

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+           <h1>About This App</h1>
+           <p className="my-1">
+            This is a full stack React app for keeping contacts
+           </p> 
+           <a href="https://abdulqudus.com" target="__blank">Developer: Jide Abdul-Qudus</a>
+        </div>
+    )
+}
+
+export default About
