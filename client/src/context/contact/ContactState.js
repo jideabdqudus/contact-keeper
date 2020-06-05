2e6005c9dc780ac9a85505a5e82853eda93cde40
jideabdqudus <jideabdqudus@gmail.com>
2020-06-05 15:00:25 +0100

    Added Changes

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -55,9 +55,13 @@ const ContactState = (props) => {
   };
 
   //Set Current Contact
-
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
   //Clear Current Contact
-
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT});
+  };
   //Update Contact
 
   //Filter Contacts
@@ -68,8 +72,11 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
         deleteContact,
+        setCurrent,
+        clearCurrent
       }}
     >
       {props.children}
