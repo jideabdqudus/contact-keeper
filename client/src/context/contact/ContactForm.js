7eae89732d7c486e32f1ae9a046aef1d7810f129
jideabdqudus <jideabdqudus@gmail.com>
2020-06-05 13:28:38 +0100

    Added Changes

---
+++ client/src/context/contact/ContactForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    type: "personal",
+  });
+
+  const { name, email, phone, type } = contact;
+
+  const onChange=(e)=>{
+    setContact({...contact, })
+  }
+  return (
+    <form>
+      <h2 className="text-primary">Add Contact</h2>
+      <input type="text" placeholder="Name" value={name} onChange={onChange} />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        placeholder="Phone"
+        value={phone}
+        onChange={onChange}
+      />
+      <h5>Contact Type</h5>
+      <input
+        type="radio"
+        name="type"
+        value="personal"
+        checked={type === "personal"}
+      />{" "}
+      Personal{" "}
+      <input
+        type="radio"
+        name="type"
+        value="professional"
+        checked={type === "professional"}
+      />{" "}
+      Professional{" "}
+      <div>
+        <input type="submit" value="Add Contact" className="btn btn-primary btn-block" />
+      </div>
+    </form>
+  );
+};
+
+export default ContactForm;
