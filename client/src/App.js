5736a0f74c9bf7e840abf0431ff95f0f7efb22fb
jideabdqudus <jideabdqudus@gmail.com>
2020-06-06 09:08:41 +0100

    Auth Context

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
-import React,{Fragment} from "react";
+import React, { Fragment } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Home from "./components/pages/Home"
-import About from "./components/pages/About"
-
-import ContactState from "./context/contact/ContactState"
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 
 const App = () => {
   return (
-    <ContactState>
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path ="/" component={Home}/>
-            <Route exact path ="/about" component={About}/>
-          </Switch>
-        </div>
-      </div>
-    </BrowserRouter>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
+      </ContactState>
+    </AuthState>
   );
 };
 
