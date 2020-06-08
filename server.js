2c57dafc14f9dbdfa5734ebc755239a04d42fc83
jideabdqudus <jideabdqudus@gmail.com>
2020-06-08 14:58:41 +0100

    Final Deployment

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-//Serve static asssets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  //Set static folder
+  // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) =>
