38b8152cdf9cff25874cb7c0c4473954c2f650f9
jideabdqudus <jideabdqudus@gmail.com>
2020-06-08 14:06:28 +0100

    FULL STACK MERN App Completed x1

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,8 +1,8 @@
 import React, { Fragment, useContext, useEffect } from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import ContactContext from "../../context/contact/ContactContext";
+import { CSSTransition } from "react-transition-group";
 import ContactItem from "./ContactItem";
 import Spinner from "../layout/Spinner";
+import ContactContext from "../../context/contact/ContactContext";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
@@ -11,16 +11,17 @@ const Contacts = () => {
 
   useEffect(() => {
     getContacts();
-    //eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
   if (contacts !== null && contacts.length === 0 && !loading) {
     return <h4>Please add a contact</h4>;
   }
+
   return (
     <Fragment>
       {contacts !== null && !loading ? (
-        <TransitionGroup>
+        <div>
           {filtered !== null
             ? filtered.map((contact) => (
                 <CSSTransition
@@ -40,7 +41,7 @@ const Contacts = () => {
                   <ContactItem contact={contact} />
                 </CSSTransition>
               ))}
-        </TransitionGroup>
+        </div>
       ) : (
         <Spinner />
       )}
