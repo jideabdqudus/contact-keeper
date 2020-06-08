63f130eda6272ff0bf57ab8773dbe6db70059cd8
jideabdqudus <jideabdqudus@gmail.com>
2020-06-08 14:49:33 +0100

    FULL STACK MERN App Completed x2 - Deploy

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 //Initialize Express
 const app = express();
 
-//Conncect DB 
+//Conncect DB
 connectDB();
 
-
 //Init Middleware
-app.use(express.json({extended: false}))
-
-//Sending data to the PORT
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the Contact-Kepper API" })
-);
+app.use(express.json({ extended: false }));
 
 //Define Routes
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//Serve static asssets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 //Set Port
 const PORT = process.env.PORT || 5000;
